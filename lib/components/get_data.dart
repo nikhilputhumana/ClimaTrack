96afd770dbfe01a5d0c273a14d4ad7319f8d2b2e
nikhilputhumana <nikhilp.csb1923@saintgits.org>
2023-08-09 21:32:51 +0530

    data for today converted as map and fetched

--- lib/components/get_data.dart
+++ lib/components/get_data.dart
@@ -38,23 +38,34 @@ Future<void> _getCoords() async {
 Future<void> fetchData() async {
   await _getCoords();
 
-  String _currDate =
-      DateFormat('yyyy-MM-dd').format(DateTime.now()); // Get current date
+  DateTime _curr = DateTime.now();
+  DateTime _next = DateTime.now().add(Duration(days: 1));
+  DateTime _aftr = DateTime.now().add(Duration(days: 2));
+
+  String _today = DateFormat('yyyy-MM-dd').format(_curr); // Get current date
+  String _tmrw = DateFormat('yyyy-MM-dd').format(_next);
+  String _dayAfter = DateFormat('yyyy-MM-dd').format(_aftr);
+
+  // print('tmrw = $_tmrw');
+  // print('day after = $_dayAfter');
 
   final response = await http.get(Uri.parse(
       'https://api.openweathermap.org/data/2.5/forecast?lat=$_lat&lon=$_lon&units=metric&cnt=24&appid=$_apiKey'));
   if (response.statusCode == 200) {
     final content = json.decode(response.body);
+    
+    _city = '${content['city']['name']}';
+    _country = '${content['city']['country']}';
+
 
     _temp = '${content['list'][0]['main']['temp']}';
     _descr = '${content['list'][0]['weather'][0]['description']}';
-    _city = '${content['city']['name']}';
-    _country = '${content['city']['country']}';
     _wind = '${content['list'][0]['wind']['speed']}';
     _humidity = '${content['list'][0]['main']['humidity']}';
     _dtdxt = '${content['list'][0]['dt_txt']}';
 
     Map<String, dynamic> dayCurr = {};
+    Map<String, dynamic> dayDetails = {};
 
     String tail = 'first';
     dayCurr[tail] = {
@@ -75,17 +86,59 @@ Future<void> fetchData() async {
       '_feels': '${content['list'][0]['main']['feels_like']}',
     };
 
-    print('Current day is $_currDate');
+    final thisDate = content['list'][0]['dt_txt'];
+    // print('This day is $thisDate');
 
+    int cnt1 = 1;
+    int cnt2 = 1;
+    int cnt3 = 1;
+
+    // -- Fetching contents and storing it on lists. --
     for (int i = 0; i < 3; i++) {
       // Complete this for loop. The loop should store in Maps. The Map has name
       // like weatherDet[day timeZone]. These Maps will be saved into sharedPrefs
       // using loops. These values will be used to get the data to home page.
+      final thisDate = content['list'][i]['dt_txt'];
+      List<String> parts = thisDate.split(' ');
+      String firstPart = parts[0];
+
+      if (firstPart == _today) {
+        dayDetails[{'1' '$cnt1'}.toString()] = {
+          'temp': '${content['list'][i]['main']['temp']}',
+          'descr': '${content['list'][i]['weather'][0]['description']}',
+          'wind': '${content['list'][i]['wind']['speed']}',
+          'dtdxt': '${content['list'][i]['dt_txt']}',
+          'humidity': '${content['list'][i]['main']['humidity']}',
+          'feels': '${content['list'][i]['main']['feels_like']}',
+        };
+        cnt1 += 1;
+      } else if (firstPart == _tmrw) {
+        dayDetails[{'2' '$cnt2'}.toString()] = {
+          'temp': '${content['list'][i]['main']['temp']}',
+          'descr': '${content['list'][i]['weather'][0]['description']}',
+          'wind': '${content['list'][i]['wind']['speed']}',
+          'dtdxt': '${content['list'][i]['dt_txt']}',
+          'humidity': '${content['list'][i]['main']['humidity']}',
+          'feels': '${content['list'][i]['main']['feels_like']}',
+        };
+        cnt2 += 1;
+      } else if (firstPart == _dayAfter) {
+        dayDetails[{'3' '$cnt3'}.toString()] = {
+          'temp': '${content['list'][i]['main']['temp']}',
+          'descr': '${content['list'][i]['weather'][0]['description']}',
+          'wind': '${content['list'][i]['wind']['speed']}',
+          'dtdxt': '${content['list'][i]['dt_txt']}',
+          'humidity': '${content['list'][i]['main']['humidity']}',
+          'feels': '${content['list'][i]['main']['feels_like']}',
+        };
+        cnt3 += 1;
+      }
     }
 
     Map<String, dynamic> retrievd = dayCurr['second'];
+    final dayCurrData = json.encode(retrievd); // Converting to string
 
-    final dayCurrData = json.encode(retrievd); // Concerting to string
+    for (int i = 1; i < cnt1; i++) {}
 
     // Storing into sharedprefs
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -102,8 +155,28 @@ Future<void> fetchData() async {
     await prefs.setString('dtdxt', _dtdxt);
     // await prefs.setString('dtdxt', dayCurr['_dtdxt']);
 
+    int k = 1;
+
+    await prefs.setString('mydata$k', 'hello');
+
     await prefs.setString(
         'myMap', dayCurrData); // Fetches the whole data. Only this is enough.
+
+    for (int i = 1; i < cnt1; i++) {
+      Map<String, dynamic> retrievd = dayDetails[{'1' '$i'}.toString()];
+      final dayData = json.encode(retrievd);
+      await prefs.setString('today$i', dayData);
+    }
+    for (int i = 1; i < cnt2; i++) {
+      Map<String, dynamic> retrievd = dayDetails[{'2' '$i'}.toString()];
+      final dayData = json.encode(retrievd);
+      await prefs.setString('tomorrow$i', dayData);
+    }
+    for (int i = 1; i < cnt3; i++) {
+      Map<String, dynamic> retrievd = dayDetails[{'3' '$i'}.toString()];
+      final dayData = json.encode(retrievd);
+      await prefs.setString('dayaftr$i', dayData);
+    }
   } else {
     print('Failed to fetch data');
     // setState(() {
--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+// import 'dart:js_interop';
 
 import 'package:flutter/material.dart';
 import 'package:predict/screens/home_model.dart';
@@ -24,6 +25,28 @@ class _ScreenHomeState extends State<ScreenHome> {
   String _humidity = '';
   String _dtdxt = '';
   String name = '';
+  String mydat = '';
+
+  // -- Today --
+  String _today1 = '';
+  String _today2 = '';
+  String _today3 = '';
+  String _today4 = '';
+  String _today5 = '';
+  String _today6 = '';
+  String _today7 = '';
+  String _today8 = '';
+
+  Map _t1map = {};
+  Map _t2map = {};
+  Map _t3map = {};
+  Map _t4map = {};
+  Map _t5map = {};
+  Map _t6map = {};
+  Map _t7map = {};
+  Map _t8map = {};
+
+  // -- Tomorow --
 
   Map decoded = {};
 
@@ -33,29 +56,70 @@ class _ScreenHomeState extends State<ScreenHome> {
   void initState() {
     super.initState();
     _getWeather();
-    // _fetchWeatherData();
   }
 
   void _getWeather() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _lat = prefs.getDouble('latitude') ?? 0.0;
+    setState(
+      () {
+        _lat = prefs.getDouble('latitude') ?? 0.0;
 
-      // final double? _lat = prefs.getDouble('latitude');
-      _lon = prefs.getDouble('longitude') ?? 0.0;
-      _temp = prefs.getString('temp') ?? 'Loading';
-      _wind = prefs.getString('wind') ?? 'Loading';
-      _humidity = prefs.getString('humidity') ?? 'Loading';
-      _city = prefs.getString('city') ?? 'Loading';
-      _country = prefs.getString('country') ?? 'Loading';
-      _descr = prefs.getString('descr') ?? 'Loading';
+        // final double? _lat = prefs.getDouble('latitude');
+        
+        _city = prefs.getString('city') ?? 'Loading';
+        _country = prefs.getString('country') ?? 'Loading';
 
-      // ---- Required ----
-      mapData = prefs.getString('myMap') ?? '';
 
-      decoded = json.decode(mapData); // converting to map
-      // _dtdxt = decoded['_dtdxt'];
-    });
+        _lon = prefs.getDouble('longitude') ?? 0.0;
+        _temp = prefs.getString('temp') ?? 'Loading';
+        _wind = prefs.getString('wind') ?? 'Loading';
+        _humidity = prefs.getString('humidity') ?? 'Loading';
+        _descr = prefs.getString('descr') ?? 'Loading';
+        mydat = prefs.getString('mydata1') ?? 'Unavailable';
+
+        // --- testing sample (not needed) ----
+        mapData = prefs.getString('myMap') ?? '';
+        decoded = json.decode(mapData); // converting to map
+
+        // --- start of today ----
+        _today1 = prefs.getString('today1') ?? '';
+        _today2 = prefs.getString('today2') ?? '';
+        _today3 = prefs.getString('today3') ?? '';
+        _today4 = prefs.getString('today4') ?? '';
+        _today5 = prefs.getString('today5') ?? '';
+        _today6 = prefs.getString('today6') ?? '';
+        _today7 = prefs.getString('today7') ?? '';
+        _today8 = prefs.getString('today8') ?? '';
+
+        _t1map = json.decode(_today1);
+
+        if (_today2 != '') {
+          _t2map = json.decode(_today2);
+          if (_today3 != '') {
+            _t3map = json.decode(_today3);
+            if (_today4 != '') {
+              _t4map = json.decode(_today4);
+              if (_today5 != '') {
+                _t5map = json.decode(_today5);
+                if (_today6 != '') {
+                  _t6map = json.decode(_today6);
+                  if (_today7 != '') {
+                    _t7map = json.decode(_today7);
+                    if (_today8 != '') {
+                      _t8map = json.decode(_today8);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        
+        // ---- end of nested if & end of today ---
+
+        // -- Tomorrow ---
+      },
+    );
   }
 
   @override
@@ -68,19 +132,20 @@ class _ScreenHomeState extends State<ScreenHome> {
         child: Column(
           children: [
             Text('This is the Home Screen'),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
             Text('Latitude is $_lat'),
             Text('Longitude is $_lon'),
-            Text('Temparature is $_temp'),
-            Text('Description is $_descr'),
+            Text('Temparature is ${_t1map['temp']}'),
+            Text('Description is ${_t1map['descr']}'),
             Text('City is $_city, $_country'),
-            Text('Wind speed $_wind'),
+            Text('Wind speed ${_t1map['wind']}'),
             Text('Humidity $_humidity'),
             Text('Dt_dxt = ${decoded['_dtdxt']}'),
+            // Text('Today2 = $_t2map'),
 
-            // Text('Name = $n'),
+
             const SizedBox(
               height: 20,
             ),
